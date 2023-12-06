864ea3443a2f39c7b7b942e205cd9233057a8f46
amanotaka <amanokuma@gmail.com>
2023-12-06 12:42:00 +0000

    in tha middle

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
-  resources :post_images,only: [:new, :index, :show, :create, :destroy,] do
+  resources :post_images,only: [:new, :index, :show, :create, :destroy, :edit] do
     resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
